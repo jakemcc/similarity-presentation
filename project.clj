72783bb9599c7caabf457f37c1985abef8fca32b
Jake McCrary <jake@jakemccrary.com>
2019-01-24 22:16:24 -0600

    upgrade lein-test-refresh

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.google.guava/guava "19.0"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.15.0"]]}})
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]]}})
